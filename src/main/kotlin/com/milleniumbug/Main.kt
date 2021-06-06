1ba4764a12cda483710cca27fb804b59d040c8fe
milleniumbug <milleniumbug@protonmail.com>
2021-06-06 17:43:54 +0200

    Fix OCR

--- src/main/kotlin/com/milleniumbug/Main.kt
+++ src/main/kotlin/com/milleniumbug/Main.kt
@@ -16,43 +16,39 @@ data class FailureStatus(val status: String)
 
 fun main(args: Array<String>) {
     val kanjiTomo = KanjiTomo2()
-    try {
-        kanjiTomo.loadData()
+    kanjiTomo.loadData()
 
-        val http = ignite().port(8080)
-        http.post("/ocr") { request, response ->
-            val bytes = request.bodyAsBytes()
-            val image = ByteArrayInputStream(bytes).use { stream ->
-                ImageIO.read(stream)
-            }
-            val areaTask = kanjiTomo.setTargetImage(image)
-            val columns = kanjiTomo.getColumns(areaTask)
-            val isVertical = columns.all { column -> column.vertical }
-            val sortedColumns =
-                if (isVertical) {
-                    columns.sortedByDescending { column -> column.rect.x }
-                } else {
-                    columns.sortedBy { column -> column.rect.y }
-                }
-            val ocrText = StringBuilder()
-            for (column in sortedColumns) {
-                val result = kanjiTomo.runOCR(areaTask, column.areas)
-                if (result != null) {
-                    ocrText.appendLine(result.bestMatchingCharacters)
-                } else {
-                    response.status(400)
-                    response.header("Content-Type", "application/json")
-                    return@post Json.encodeToString(serializer<FailureStatus>(), FailureStatus("failure"))
-                }
-            }
-
-            response.status(200)
-            response.header("Content-Type", "application/json")
-            Json.encodeToString(serializer<SuccessStatus>(), SuccessStatus("success", ocrText.toString(), isVertical))
+    val http = ignite().port(8080)
+    http.post("/ocr") { request, response ->
+        val bytes = request.bodyAsBytes()
+        val image = ByteArrayInputStream(bytes).use { stream ->
+            ImageIO.read(stream)
         }
-    }
-    finally {
-        kanjiTomo.close()
+        val areaTask = kanjiTomo.setTargetImage(image)
+        val columns = kanjiTomo.getColumns(areaTask)
+        val isVertical = columns.all { column -> column.vertical }
+        val sortedColumns =
+            if (isVertical) {
+                columns.sortedByDescending { column -> column.rect.x }
+            } else {
+                columns.sortedBy { column -> column.rect.y }
+            }
+        val ocrText = StringBuilder()
+        for (column in sortedColumns) {
+            val result = kanjiTomo.runOCR(areaTask, column.areas)
+            if (result != null) {
+                ocrText.appendLine(result.bestMatchingCharacters)
+            }
+            else {
+                response.status(400)
+                response.header("Content-Type", "application/json")
+                return@post Json.encodeToString(serializer<FailureStatus>(), FailureStatus("failure"))
+            }
+        }
+
+        response.status(200)
+        response.header("Content-Type", "application/json")
+        Json.encodeToString(serializer<SuccessStatus>(), SuccessStatus("success", ocrText.toString(), isVertical))
     }
 }
 
